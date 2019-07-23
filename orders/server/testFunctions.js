3027860028b9196d736fca104772fb183195b82c
Jerome O'Mahony <jom_ie@yahoo.ie>
2019-07-23 15:57:00 +0100

    Added CRUD for the orders contract

---
+++ orders/server/testFunctions.js
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const network = require('./network');
+
+const order = 
+  {
+    orderId: '1005',
+    docType: 'foodOrder',
+    despatchDate: '2019-07-20',
+    dateReceived: '2019-07-20',
+    buyerId: '1001',
+    quantity: '750',
+    qtyUnitMeasurement: 'Kg',
+    processUsed: 'Smoking',
+    orderLines: [
+      { orderLine: '1', sourceBatchId: '101', tested: 'Yes', quantity: '500', qtyUnitMeasurement: 'Kg' },
+      { orderLine: '2', sourceBatchId: '102', tested: 'Yes', quantity: '250', qtyUnitMeasurement: 'Kg' },
+    ],
+  };
+
+// network.foodSupplyAssetExists("1005")  ;
+
+// network.createOrder(order.orderId, JSON.stringify(order));
+
+// network.updateOrder(order.orderId, JSON.stringify(order));
+
+// network.deleteOrder("1005");
