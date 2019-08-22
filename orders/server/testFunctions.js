07f4f7636074c43426311157826facb0b1fa12c7
John Doe <johndoe@example.com>
2019-08-22 11:39:42 +0100

    No major changes - just modified slightly while trying to get local fabric to work.

--- orders/server/testFunctions.js
+++ orders/server/testFunctions.js
@@ -6,56 +6,69 @@
 
 const fs = require("fs");
 const path = require("path");
-const StringBuilder = require('node-stringbuilder');
+const StringBuilder = require("node-stringbuilder");
 const sb = new StringBuilder();
 
 const network = require("./network");
 
-const order = 
-  {
-    "orderId": "1005",
-    "docType": "foodOrder",
-    "despatchDate": "2019-07-20",
-    "dateReceived": "2019-07-20",
-    "buyerId": "1001",
-    "quantity": 752,
-    "qtyUnitMeasurement": "Kg",
-    "processUsed": "Smoking Hot Dude",
-    "orderLines": [
-      {
-      "orderLine": 1, "sourceorderId": "101", "tested": "Yes", "quantity": 500, "qtyUnitMeasurement": "Kg" },
-      { "orderLine": 2, "sourceorderId": "102", "tested": "Yes", "quantity": 250, "qtyUnitMeasurement": "Kg" },
-    ],
-  };
+const order = {
+    orderId: "1003",
+    docType: "foodOrder",
+    despatchDate: "2019-07-20",
+    dateReceived: "2019-07-20",
+    buyerId: "1001",
+    quantity: 752,
+    qtyUnitMeasurement: "Kg",
+    processUsed: "Smokin",
+    orderLines: [
+        {
+            orderLine: 1,
+            sourceorderId: "101",
+            tested: "Yes",
+            quantity: 500,
+            qtyUnitMeasurement: "Kg"
+        },
+        {
+            orderLine: 2,
+            sourceorderId: "102",
+            tested: "Yes",
+            quantity: 250,
+            qtyUnitMeasurement: "Kg"
+        }
+    ]
+};
 
 const testy = async () => {
+    const res = await network.getHistoryForKey(order.orderId);
 
-  const res = await network.getHistoryForKey(order.orderId);
+    let allResults = "";
+    const js = JSON.parse(res);
+    //Convert the array of ASCII values into a character string
+    js.data
+        .toString()
+        .split(",")
+        .forEach(s => sb.append(String.fromCharCode(parseInt(s, 10))));
+    //Parse the string back into a JSON object
+    const myJson = JSON.parse(sb.toString());
 
-  let allResults = "";
-  const js = JSON.parse(res);
-  //Convert the array of ASCII values into a character string
-  js.data.toString().split(',').forEach((s) => (sb.append(String.fromCharCode(parseInt(s, 10)))));
-  //Parse the string back into a JSON object
-  const myJson = JSON.parse(sb.toString());
+    //Now all items in the history object array can be accessed
+    for (var myKey in myJson) {
+        console.log(myJson[myKey].Value);
+        const dt = new Date(
+            parseInt(myJson[myKey].Timestamp.seconds.low) * 1000
+        );
+        // console.log(myJson[myKey].Timestamp.seconds.low);
+        console.log(dt.toISOString());
 
-  //Now all items in the history object array can be accessed
-  for (var myKey in myJson) {
-    // console.log(myJson[myKey].Value);
-    const dt = new Date(parseInt(myJson[myKey].Timestamp.seconds.low) * 1000);
-    // console.log(myJson[myKey].Timestamp.seconds.low);
-    console.log(dt.toISOString());
-
-    console.log("--------------------------------------------------------------------");
-
-  }
-
-}
+        console.log(
+            "--------------------------------------------------------------------"
+        );
+    }
+};
 
 testy();
 
-
-// network.foodOrderExists("1001")  ;
+// network.foodOrderExists("1001");
 
 // network.createOrder(order.orderId, JSON.stringify(order));
 
